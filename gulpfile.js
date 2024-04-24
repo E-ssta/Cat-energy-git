0b09119759b5622b816ebc4765b1d85fa2ae7010
Науменко Татьяна Александровна <tnaumenko@daichi.local>
2024-04-24 18:27:38 +0300

    сборка +

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ import plumber from 'gulp-plumber';
 import less from 'gulp-less';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
-import browser from 'browser-sync';
+// import browser from 'browser-sync';
 import htmlmin from 'gulp-htmlmin';
 import csso from 'postcss-csso';
 import rename from 'gulp-rename';
@@ -26,12 +26,12 @@ export const styles = () => {
     ]))
     .pipe(rename("style.min.css"))
     .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
-    .pipe(browser.stream());
+    .pipe(sync.stream());
 }
 
 // HTML
 
-const html = () => {
+export const html = () => {
   return gulp.src("source/*.html")
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest("build"));
@@ -40,29 +40,30 @@ const html = () => {
 
 // Scripts
 
-const scripts = () => {
+export const scripts = () => {
   return gulp.src("source/js/*.js")
     .pipe(terser())
     .pipe(rename("app.min.js"))
     .pipe(gulp.dest("build/js"))
+    .pipe(sync.stream());
 }
 
 // Images
 
-const optimizeImages = () => {
+export const optimizeImages = () => {
   return gulp.src("source/img/**/*.{png,jpg,svg}")
     .pipe(squoosh())
     .pipe(gulp.dest("build/img"))
 }
 
-const copyImages = () => {
+export const copyImages = () => {
   return gulp.src("source/img/**/*.{png,jpg,svg}")
     .pipe(gulp.dest("build/img"))
 }
 
 // WebP
 
-const createWebp = () => {
+export const createWebp = () => {
   return gulp.src("source/img/**/*.{jpg,png}")
     .pipe(webp({quality: 90}))
     .pipe(gulp.dest("build/img"))
@@ -70,7 +71,7 @@ const createWebp = () => {
 
 // Sprite
 
-const sprite = () => {
+export const sprite = () => {
   return gulp.src("source/img/icons/*.svg")
     .pipe(svgstore({
       inlineSvg: true
@@ -81,11 +82,12 @@ const sprite = () => {
 
 // Copy
 
-const copy = (done) => {
+export const copy = (done) => {
   gulp.src([
     "source/fonts/*.{woff2,woff}",
     "source/*.ico",
     "source/img/**/*.svg",
+    "source/*.webmanifest",
     "!source/img/icons/*.svg",
   ], {
     base: "source"
@@ -97,16 +99,20 @@ const copy = (done) => {
 // Clean
 
 export const clean = () => {
-  // return del("build");
-
   return deleteAsync("build");
 }
 
+// Reload
+
+const reload = (done) => {
+  sync.reload();
+  done();
+}
 
 // Server
 
 const server = (done) => {
-  browser.init({
+  sync.init({
     server: {
       baseDir: 'build'
     },
@@ -117,20 +123,13 @@ const server = (done) => {
   done();
 }
 
-// Reload
-
-const reload = (done) => {
-  sync.reload();
-  done();
-}
-
 // Watcher
 
 const watcher = () => {
-  gulp.watch('source/less/**/*.less', gulp.series(styles));
-  gulp.watch('source/js/**/*.js', gulp.series(scripts, reload));
-  gulp.watch('source/*.html', gulp.series(html, reload));
-  gulp.watch('source/*.html').on('change', browser.reload);
+  gulp.watch('source/sass/**/*.scss', gulp.series(styles));
+  gulp.watch("source/js/script.js", gulp.series(scripts));
+  // gulp.watch('source/*.html').on('change', sync.reload); // заменил эту строку из обновления от Кекса в разделе препроцессоров на нижнюю, так как при каждом изменении html-файлов нужно запускать теперь еше функцию минификации html.
+  gulp.watch("source/*.html", gulp.series(html, reload));
 }
 
 // Build
